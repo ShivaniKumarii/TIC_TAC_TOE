73b1dcf56be17642f6b337e5be3af2c81bccd1fd
Shivani Kumari <123109161+ShivaniKumarii@users.noreply.github.com>
2023-07-31 01:51:21 +0530

    Add files via upload

---
+++ TIC_TAC_TOE_GAME/src/main/java/com/mycompany/tic_tac_toe_game/plr1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tic_tac_toe_game;
+
+/**
+ *
+ * @author KIIT
+ */
+class plr1 {
+
+    static void setText(String valueOf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ TIC_TAC_TOE_GAME/src/main/java/com/mycompany/tic_tac_toe_game/txtbtn1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tic_tac_toe_game;
+
+/**
+ *
+ * @author KIIT
+ */
+class txtbtn1 {
+    
+}
